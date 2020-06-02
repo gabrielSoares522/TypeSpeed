543f07c7a88692930a9dcc564ff33a49d03154d2
gabriel soares <gabrielcsoares52@gmail.com>
2020-06-02 16:29:11 -0300

    adicionando app

---
+++ js/app.js
@@ -0,0 +1,75 @@
+const frases = ["ola meu nome e diego","compre uma banana","leia o livro recomendado"];
+let objetivo = document.getElementById("txtObjetivo");
+let campo = document.getElementById("txtCampo");
+let resultado = document.getElementById("resultado");
+let medidor = document.getElementById("medidor");
+let inicio;
+let fim;
+let terminou = false;
+
+var cronometro=3;
+var cronInicio;
+function checar() {
+    var texto = objetivo.innerText;
+    var escrevendo =  campo.value;
+    if(texto.substring(0,escrevendo.length) == escrevendo){
+        campo.style.color = "black";
+        var novoMedidor = (100*(texto.length-(texto.length-escrevendo.length)))/texto.length;
+        medidor.style.width = novoMedidor.toString()+"%";
+    }
+    else{
+        campo.style.color = "red";
+        console.log('errado');
+    }
+    if(texto == escrevendo && terminou==false){
+        campo.style.display= "none";
+        terminou = true;
+        fim = new Date();
+        let diferenca = fim-inicio;
+        diferenca /= 1000;
+                
+        var segundos = Math.round(diferenca);
+        var resu = texto.length/diferenca;
+                
+        var tempoPlacar= document.createElement("p");
+        var tempoRelativo= document.createElement("p");
+        var recordePlacar= document.createElement("p");
+        var recorde =parseFloat(localStorage.getItem("recorde")) || resu-1;
+                
+        if(resu>recorde){
+            recorde = resu;
+            localStorage.setItem("recorde",recorde);
+            console.log(recorde);
+        }
+        tempoPlacar.innerHTML = "total: " + diferenca.toFixed(4) + " segundos";
+        tempoRelativo.innerHTML = "velocidade: " + resu.toFixed(4) + " L/S";
+        recordePlacar.innerHTML = "recorde: " + recorde.toFixed(4) + " L/s";
+        resultado.appendChild(tempoPlacar);
+        resultado.appendChild(tempoRelativo);
+        resultado.appendChild(recordePlacar);
+    }
+}
+
+function novaFrase() {
+    terminou = false;
+    campo.value = "";
+    campo.style.display= "none";
+    resultado.innerHTML="";
+    objetivo.innerText=frases[Math.floor(Math.random()*frases.length)];
+    cronometro=3;
+    cronInicio = setInterval(inicializando,1000);
+}
+
+function inicializando() {
+    if(cronometro==0) {
+        campo.style.display= "block";
+        campo.focus();
+        resultado.innerHTML = '';
+        inicio = new Date();
+        clearInterval(cronInicio)
+    }
+    else{
+        resultado.innerHTML = cronometro;
+        cronometro--;
+    }
+}
